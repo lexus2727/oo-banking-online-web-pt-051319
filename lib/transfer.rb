8673b1283ef746bc8100de6018f5e6125c587540
Candice Parker <lexus2424@gmail.com>
2019-06-09 21:51:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,7 +37,7 @@ class Transfer
 end
 
 def reverse_transfer
-  if @status = "complete"
+  if @status == "complete"
   @sender.deposit(@amount)
   @receiver.deposit(@amount * -1)
   @status = "reversed"
